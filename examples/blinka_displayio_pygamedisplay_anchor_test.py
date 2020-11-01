9be94a221c77927c2500185a4b34aebc2a567621
foamyguy <foamyguy@gmail.com>
2020-10-31 21:24:47 -0500

    black format anchor example

--- examples/blinka_displayio_pygamedisplay_anchor_test.py
+++ examples/blinka_displayio_pygamedisplay_anchor_test.py
@@ -14,7 +14,7 @@ display = PyGameDisplay(width=1770, height=920)
 text_area = label.Label(terminalio.FONT, text="Hello world", scale=3)
 
 text_area.anchor_point = (0.5, 0.5)
-text_area.anchored_position = (display.width//2, display.height//2)
+text_area.anchored_position = (display.width // 2, display.height // 2)
 print(text_area.bounding_box)
 print(f"{text_area.x}, {text_area.y}")
 main_group = displayio.Group()
